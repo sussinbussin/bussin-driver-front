cd80e062dfa7640b398849e1782a18cc9ecaf62c
jz <t.jingzhi1@gmail.com>
2022-10-07 19:40:16 +0800

    Added Dark theme for map and scheduled button

--- components/HomeTopBar.js
+++ components/HomeTopBar.js
@@ -19,7 +19,19 @@ const HomeTopBar = () => {
       }}
       flexDirection="row"
     >
-      <Box style={{ flex: 1, backgroundColor: 0 }}></Box>
+      <Box style={{ flex: 1, backgroundColor: 0 }}>
+        <Pressable onPress={handleNavigate}>
+          <AntDesign
+            name="calendar"
+            size={28}
+            color="white"
+            style={{
+              textAlign: "left",
+              marginLeft: 20,
+            }}
+          />
+        </Pressable>
+      </Box>
       <VStack style={{ height: 50, flex: 1 }}>
         <Heading style={{ flex: 1, textAlign: "center" }}>Bussin.</Heading>
         <Text fontSize="xs" style={{ flex: 1, textAlign: "center" }}>
